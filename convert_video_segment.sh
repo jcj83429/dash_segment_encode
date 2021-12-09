a09b318e1697c7efa20992d030ef3aba10d211a7
jcj83429 <jcj83429@gmail.com>
2021-12-08 22:43:27 -0800

    video encoding: don't seek when start time is 0
    avoid seeking problem in some flv files

--- convert_video_segment.sh
+++ convert_video_segment.sh
@@ -15,6 +15,11 @@ lockfile="$basesegpath.lock"
 if flock -xn 200; then
 	echo got lock
 	if [[ ! -f "$basesegpath".webm ]]; then
+		seekflags=""
+		if [[ $ss -gt 0 ]]; then
+			seekflags="-ss $ss"
+		fi
+
 		# for vp9
 		curtime=$(date '+%F %H-%M-%S')
 		vf="yadif=mode=send_frame:deint=interlaced,scale=$w:$h,drawtext=text=$curtime:fontcolor=white:box=1:boxcolor=black@0.5:boxborderw=5:x=5:y=5"
@@ -24,7 +29,7 @@ if flock -xn 200; then
 		#codecflags="-c:v libsvtav1 -qp 20 -preset 12"
 
 		# copyts, vsync and enc_time_base are needed to handle videos with fractional framerate or small errors in timestamps better and avoid dropping or duping frames on segment boundaries.
-		timeout -k 9 60 ffmpeg -ss "$ss" -i "$infile" -vf "$vf" -t "$t" -copyts -start_at_zero -vsync passthrough -enc_time_base -1 -an -sn -map_metadata -1 $codecflags -g 1000 -keyint_min 1000 -dash 1 -dash_segment_type webm -init_seg_name "$basesegname"_init.webm -media_seg_name "$basesegname".webm "$basesegpath".mpd
+		timeout -k 9 60 ffmpeg $seekflags -i "$infile" -vf "$vf" -t "$t" -copyts -start_at_zero -vsync passthrough -enc_time_base -1 -an -sn -map_metadata -1 $codecflags -g 1000 -keyint_min 1000 -dash 1 -dash_segment_type webm -init_seg_name "$basesegname"_init.webm -media_seg_name "$basesegname".webm "$basesegpath".mpd
 		rm "$basesegpath".mpd
 		mv "$basesegpath"_init.webm "$baseinitpath".webm
 		# no need to patch the timestamp. copyts generates segments with the correct timestamp.
