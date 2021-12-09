7a46d2f7f9efdf9a3d2a1f83cc701cae890ee9c7
jcj83429 <jcj83429@gmail.com>
2021-12-08 22:40:41 -0800

    add timeout to ffmpeg command
    when using svt av1 and no frames are encoded because of seeking problems (seen on some flv files), ffmpeg can get stuck forever

--- convert_video_segment.sh
+++ convert_video_segment.sh
@@ -24,7 +24,7 @@ if flock -xn 200; then
 		#codecflags="-c:v libsvtav1 -qp 20 -preset 12"
 
 		# copyts, vsync and enc_time_base are needed to handle videos with fractional framerate or small errors in timestamps better and avoid dropping or duping frames on segment boundaries.
-		ffmpeg -ss "$ss" -i "$infile" -vf "$vf" -t "$t" -copyts -start_at_zero -vsync passthrough -enc_time_base -1 -an -sn -map_metadata -1 $codecflags -g 1000 -keyint_min 1000 -dash 1 -dash_segment_type webm -init_seg_name "$basesegname"_init.webm -media_seg_name "$basesegname".webm "$basesegpath".mpd
+		timeout -k 9 60 ffmpeg -ss "$ss" -i "$infile" -vf "$vf" -t "$t" -copyts -start_at_zero -vsync passthrough -enc_time_base -1 -an -sn -map_metadata -1 $codecflags -g 1000 -keyint_min 1000 -dash 1 -dash_segment_type webm -init_seg_name "$basesegname"_init.webm -media_seg_name "$basesegname".webm "$basesegpath".mpd
 		rm "$basesegpath".mpd
 		mv "$basesegpath"_init.webm "$baseinitpath".webm
 		# no need to patch the timestamp. copyts generates segments with the correct timestamp.
